a743a8bc69f6e3030c228cd1e3d7707235ad2234
Sourav Saha <ssuman@1313@gmail.com>
2026-07-24 19:03:15 +0530

    upated Jnunit reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,10 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['list']
+    ['list'],
+    ['junit', {
+    outputFile: 'test-results/results.xml'
+    }]
 ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
